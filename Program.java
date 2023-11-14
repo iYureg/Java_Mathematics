ed2b2b2cf2111fca95f1a1a71d11434f6476f3e7
Yurii <iyureg@gmail.com>
2023-11-14 14:42:18 +0300

    Program.java code realisation

--- Program.java
+++ Program.java
@@ -3,8 +3,17 @@
  * entry point
  * class Program
  */
+
+import Models.Sum;
+import Presentation.Presenter;
+import View.View;
+
 public class Program {
     public static void main(String[] args) {
+        Presenter p = new Presenter(new Sum(), new View());
 
+        p.handler();
+        p.handler();
+        p.handler();
     }
 }
