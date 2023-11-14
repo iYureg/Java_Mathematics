a34b08e4b8f617f9a6dd871723c4825dcf9b1bcd
Yurii <iyureg@gmail.com>
2023-11-14 14:14:46 +0300

    added class View

---
+++ View/View.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class View {
+    Scanner in = new Scanner(System.in);
+
+    public int getValue(String s) {
+        System.out.printf("%s", s);
+        return in.nextInt();
+    }
+
+    public void print(int data, String s) {
+        System.out.printf("%s %d\n", s, data);
+    }
+}
